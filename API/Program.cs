b9597dfdf1008236787dc41c44666b8ad959a7db
Abdullah-Arab10 <abobassam14@gmail.com>
2023-04-27 12:38:45 +0300

    git ignore

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
+using Azure.Identity;
 using Loza.API.Extenstions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.RegisterServices(typeof(Program));
+
+
+
 
 var app = builder.Build();
 
